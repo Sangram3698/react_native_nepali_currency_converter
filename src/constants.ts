9e7106ecf55b7830d92a9c86a733877c17f1a6e6
Sangram3698 <sngrmbhandari@gmail.com>
2024-05-17 14:53:24 +0545

    Complete Currency app

---
+++ src/constants.ts
@@ -0,0 +1,57 @@
+/* eslint-disable prettier/prettier */
+export const currencyByRupee: Currency[] = [
+  {
+    name: 'DOLLAR',
+    value: 0.0075,
+    flag: '🇺🇸',
+    symbol: '$',
+  },
+  {
+    name: 'EURO',
+    value: 0.0069,
+    flag: '🇪🇺',
+    symbol: '€',
+  },
+  {
+    name: 'POUND',
+    value: 0.0059,
+    flag: '🇬🇧',
+    symbol: '£',
+  },
+  {
+    name: 'RUBEL',
+    value: 0.68,
+    flag: '🇷🇺',
+    symbol: '₽',
+  },
+  {
+    name: 'AUS DOLLAR',
+    value: 0.011,
+    flag: '🇦🇺',
+    symbol: 'A$',
+  },
+  {
+    name: 'CAN DOLLAR',
+    value: 0.010,
+    flag: '🇨🇦',
+    symbol: 'C$',
+  },
+  {
+    name: 'YEN',
+    value: 1.17,
+    flag: '🇯🇵',
+    symbol: '¥',
+  },
+  {
+    name: 'DINAR',
+    value: 0.0023,
+    flag: '🇰🇼',
+    symbol: 'KD',
+  },
+  {
+    name: 'INDIAN',
+    value: 0.62,
+    flag: 'IND',
+    symbol: '₹',
+  },
+];
